42a6ec2fb1f1528ecf4cc0e4d1259f93513a8639
Maciej Obarski <mobarski@gmail.com>
2023-08-24 23:25:46 +0200

    pal

--- vm/palettes.py
+++ vm/palettes.py
@@ -1,8 +1,8 @@
 palettes = [
-	// === 16 colors ===================================================
+	# === 16 colors ===================================================
 	{
-		url: 'https://lospec.com/palette-list/endesga-16',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/endesga-16',
+		'rgb': [
 			'#e4a672',
 			'#b86f50',
 			'#743f39',
@@ -18,12 +18,12 @@ palettes = [
 			'#afbfd2',
 			'#ffffff',
 			'#2ce8f4',
-			'#0484d1'
+			'#0484d1',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/sweetie-16',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/sweetie-16',
+		'rgb': [
 			'#1a1c2c',
 			'#5d275d',
 			'#b13e53',
@@ -39,12 +39,12 @@ palettes = [
 			'#f4f4f4',
 			'#94b0c2',
 			'#566c86',
-			'#333c57'
+			'#333c57',
 		]
 	},
 	{
-		url : 'https://lospec.com/palette-list/na16',
-		rgb : [
+		'url': 'https://lospec.com/palette-list/na16',
+		'rgb': [
 			'#8c8fae',
 			'#584563',
 			'#3e2137',
@@ -60,12 +60,12 @@ palettes = [
 			'#7ec4c1',
 			'#34859d',
 			'#17434b',
-			'#1f0e1c'
+			'#1f0e1c',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/cartoon-army',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/cartoon-army',
+		'rgb': [
 			'#201616',
 			'#37403b',
 			'#4d6a89',
@@ -81,13 +81,13 @@ palettes = [
 			'#b07032',
 			'#e6c369',
 			'#728933',
-			'#aade6e'
+			'#aade6e',
 		]
 	},
-	// === 15 colors ===================================================
+	# 15 colors ===================================================
 	{
-		url: 'https://lospec.com/palette-list/15p-dx',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/15p-dx',
+		'rgb': [
 			'#6e3232',
 			'#bb5735',
 			'#df9245',
@@ -102,12 +102,12 @@ palettes = [
 			'#c7b295',
 			'#87715b',
 			'#463f3c',
-			'#201708'
+			'#201708',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/generic-15',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/generic-15',
+		'rgb': [
 			'#222428',
 			'#5c596f',
 			'#a59ab6',
@@ -122,12 +122,12 @@ palettes = [
 			'#7ca230',
 			'#2b51a7',
 			'#169ac8',
-			'#81d5d7'
+			'#81d5d7',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/bit-interior',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/bit-interior',
+		'rgb': [
 			'#f3e2b1',
 			'#f7c439',
 			'#ff880b',
@@ -142,12 +142,12 @@ palettes = [
 			'#929992',
 			'#2e2823',
 			'#331b0b',
-			'#201208'
+			'#201208',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/consistent-15',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/consistent-15',
+		'rgb': [
 			'#80dbf8',
 			'#6079dc',
 			'#5d4483',
@@ -162,12 +162,12 @@ palettes = [
 			'#eb795d',
 			'#913d5c',
 			'#2e2549',
-			'#030305'
+			'#030305',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/pastel-refinado',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/pastel-refinado',
+		'rgb': [
 			'#545454',
 			'#7e7e7e',
 			'#d2d2d2',
@@ -182,13 +182,13 @@ palettes = [
 			'#89b37d',
 			'#b8f5d4',
 			'#8fccc5',
-			'#6b8ea3'
+			'#6b8ea3',
 		]
 	},
-	// === 14 colors ===================================================
+	# 14 colors ===================================================
 	{
-		url: 'https://lospec.com/palette-list/zencillo14',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/zencillo14',
+		'rgb': [
 			'#1a1226',
 			'#515270',
 			'#8498ab',
@@ -202,12 +202,12 @@ palettes = [
 			'#f5805d',
 			'#ffe785',
 			'#7fd66f',
-			'#3ba150'
+			'#3ba150',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/nostalgi14',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/nostalgi14',
+		'rgb': [
 			'#261f33',
 			'#3c3f4f',
 			'#717882',
@@ -221,12 +221,12 @@ palettes = [
 			'#855967',
 			'#663a5e',
 			'#7b4c7d',
-			'#b07994'
+			'#b07994',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/dungeon14',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/dungeon14',
+		'rgb': [
 			'#191520',
 			'#2a2d3c',
 			'#294566',
@@ -240,13 +240,13 @@ palettes = [
 			'#9653a2',
 			'#723738',
 			'#c68c76',
-			'#f0efdf'
+			'#f0efdf',
 		]
 	},
-	// === 12 colors ===================================================
+	# 12 colors ===================================================
 	{
-		url: 'https://lospec.com/palette-list/japanese-woodblock',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/japanese-woodblock',
+		'rgb': [
 			'#2b2821',
 			'#624c3c',
 			'#d9ac8b',
@@ -258,13 +258,13 @@ palettes = [
 			'#b03a48',
 			'#d4804d',
 			'#e0c872',
-			'#3e6958'
+			'#3e6958',
 		]
 	},
-	// ==== 10 colors ==================================================
+	#= 10 colors ==================================================
 	{
-		url: 'https://lospec.com/palette-list/cromatica',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/cromatica',
+		'rgb': [
 			'#fbf8fd',
 			'#a1a9d1',
 			'#007fff',
@@ -274,12 +274,12 @@ palettes = [
 			'#fd1a43',
 			'#ffb16c',
 			'#fede5b',
-			'#74ead6'
+			'#74ead6',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/ty-quiet-night-10',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/ty-quiet-night-10',
+		'rgb': [
 			'#e0c79f',
 			'#a6aa62',
 			'#b1863e',
@@ -289,12 +289,12 @@ palettes = [
 			'#6c755b',
 			'#5c6878',
 			'#3a3b3e',
-			'#2a1d17'
+			'#2a1d17',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/what-the-dec',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/what-the-dec',
+		'rgb': [
 			'#fffdce',
 			'#ffcd6b',
 			'#78da22',
@@ -304,13 +304,13 @@ palettes = [
 			'#38153b',
 			'#a6ced2',
 			'#4796cf',
-			'#204075'
+			'#204075',
 		]
 	},
-	// === 9 colors ===================================================
+	# 9 colors ===================================================
 	{
-		url: 'https://lospec.com/palette-list/reteocal-9',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/reteocal-9',
+		'rgb': [
 			'#7aadb4',
 			'#364e57',
 			'#3aa64f',
@@ -319,13 +319,13 @@ palettes = [
 			'#77433e',
 			'#3e1d3d',
 			'#a2304d',
-			'#748e86'
+			'#748e86',
 		]
 	},
-	// === 8 colors ===================================================
+	# 8 colors ===================================================
 	{
-		url: 'https://lospec.com/palette-list/nostalgic-dreams',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/nostalgic-dreams',
+		'rgb': [
 			'#d9af80',
 			'#b07972',
 			'#524352',
@@ -333,12 +333,12 @@ palettes = [
 			'#7f9bb0',
 			'#bfd4b0',
 			'#90b870',
-			'#628c70'
+			'#628c70',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/retrocal-8',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/retrocal-8',
+		'rgb': [
 			'#6eb8a8',
 			'#2a584f',
 			'#74a33f',
@@ -346,12 +346,12 @@ palettes = [
 			'#c6505a',
 			'#2f142f',
 			'#774448',
-			'#ee9c5d'
+			'#ee9c5d',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/matriax8c',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/matriax8c',
+		'rgb': [
 			'#f0f0dc',
 			'#fac800',
 			'#10c840',
@@ -359,12 +359,12 @@ palettes = [
 			'#d24040',
 			'#a0694b',
 			'#736464',
-			'#101820'
+			'#101820',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/endesga-8',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/endesga-8',
+		'rgb': [
 			'#fdfdf8',
 			'#d32734',
 			'#da7d22',
@@ -372,12 +372,12 @@ palettes = [
 			'#28c641',
 			'#2d93dd',
 			'#7b53ad',
-			'#1b1c33'
+			'#1b1c33',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/prospecal-8',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/prospecal-8',
+		'rgb': [
 			'#6db5ba',
 			'#26544c',
 			'#76aa3a',
@@ -385,12 +385,12 @@ palettes = [
 			'#d23c4f',
 			'#2b1328',
 			'#753d38',
-			'#efad5f'
+			'#efad5f',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/fairydust-8',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/fairydust-8',
+		'rgb': [
 			'#f0dab1',
 			'#e39aac',
 			'#c45d9f',
@@ -398,12 +398,12 @@ palettes = [
 			'#6461c2',
 			'#2ba9b4',
 			'#93d4b5',
-			'#f0f6e8'			
+			'#f0f6e8',		
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/islandshore',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/islandshore',
+		'rgb': [
 			'#2c2111',
 			'#584e44',
 			'#2b5e91',
@@ -411,12 +411,12 @@ palettes = [
 			'#a17b5b',
 			'#5ba1a7',
 			'#cbb234',
-			'#ded598'
+			'#ded598',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/crayon-paint',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/crayon-paint',
+		'rgb': [
 			'#76a1f1',
 			'#2f6a5d',
 			'#69bf2b',
@@ -424,181 +424,175 @@ palettes = [
 			'#e98e33',
 			'#a03030',
 			'#151212',
-			'#9e969b'
+			'#9e969b',
 		]
 	},
-	// === 7 colors ===================================================
+	# 7 colors ===================================================
 	{
-		url: 'https://lospec.com/palette-list/pastel-qt',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/pastel-qt',
+		'rgb': [
 			'#cb8175',
 			'#e2a97e',
 			'#f0cf8e',
 			'#f6edcd',
 			'#a8c8a6',
 			'#6d8d8a',
-			'#655057'
+			'#655057',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/rabbit',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/rabbit',
+		'rgb': [
 			'#d47564',
 			'#e8c498',
 			'#ecece0',
 			'#4fa4a5',
 			'#aad395',
 			'#3b324a',
-			'#5c6182'
+			'#5c6182',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/tarot7',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/tarot7',
+		'rgb': [
 			'#292829',
 			'#a65a66',
 			'#748530',
 			'#677b80',
 			'#c4a44d',
 			'#bdb099',
-			'#d6c0ae'
+			'#d6c0ae',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/s-lucky',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/s-lucky',
+		'rgb': [
 			'#ab6187',
 			'#ebc686',
 			'#d8e8e6',
 			'#65db73',
 			'#709dcf',
 			'#5a687d',
-			'#211e33'
+			'#211e33',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/mini-7',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/mini-7',
+		'rgb': [
 			'#30303d',
 			'#5564a0',
 			'#61b7ac',
 			'#ede4da',
 			'#f7b33d',
 			'#ea6e48',
-			'#818747'
+			'#818747',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/discord-2021',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/discord-2021',
+		'rgb': [
 			'#ed4245',
 			'#fee75c',
 			'#57f287',
 			'#5865f2',
 			'#eb459e',
 			'#ffffff',
-			'#000000'
+			'#000000',
 		]
 	},
+	# 6 colors ===================================================
 	{
-		url: '',
-		rgb: [
-
-		]
-	},
-	// === 6 colors ===================================================
-	{
-		url: 'https://lospec.com/palette-list/curiosities',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/curiosities',
+		'rgb': [
 			'#46425e',
 			'#15788c',
 			'#00b9be',
 			'#ffeecc',
 			'#ffb0a3',
-			'#ff6973'
+			'#ff6973',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/oil-6',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/oil-6',
+		'rgb': [
 			'#fbf5ef',
 			'#f2d3ab',
 			'#c69fa5',
 			'#8b6d9c',
 			'#494d7e',
-			'#272744'
+			'#272744',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/carpet-rose',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/carpet-rose',
+		'rgb': [
 			'#3b392a',
 			'#655d4a',
 			'#6f4143',
 			'#a46a66',
 			'#a4825d',
-			'#d6baa5'
+			'#d6baa5',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/puffball-6',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/puffball-6',
+		'rgb': [
 			'#eedbc8',
 			'#e0bb68',
 			'#97b34e',
 			'#d58353',
 			'#548b71',
-			'#5a473e'
+			'#5a473e',
 		]
 	},
-	// === 5 colors ===================================================
+	# 5 colors ===================================================
 	{
-		url: 'https://lospec.com/palette-list/twilight-5',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/twilight-5',
+		'rgb': [
 			'#fbbbad',
 			'#ee8695',
 			'#4a7a96',
 			'#333f58',
-			'#292831'			
+			'#292831',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/slimy-05',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/slimy-05',
+		'rgb': [
 			'#d1cb95',
 			'#40985e',
 			'#1a644e',
 			'#04373b',
-			'#0a1a2f'
+			'#0a1a2f',
 		]
 	},
 	{
-		url: 'https://lospec.com/palette-list/retrotronic',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/retrotronic',
+		'rgb': [
 			'#392b35',
 			'#7a9c96',
 			'#486b7f',
 			'#d1bfb0',
-			'#bb474f'
+			'#bb474f',
 		]
 	},
-	// === 4 colors ===================================================
+	# 4 colors ===================================================
 	{
-		url: 'https://lospec.com/palette-list/2bit-demichrome',
-		rgb: [
+		'url': 'https://lospec.com/palette-list/2bit-demichrome',
+		'rgb': [
 			'#211e20',
 			'#555568',
 			'#a0a08b',
-			'#e9efec'
+			'#e9efec',
 		]
 	},
-	// === TEMPLATE ===================================================
+	# TEMPLATE ===================================================
 	{
-		url: '',
-		rgb: [
+		'url': '',
+		'rgb': [
 
 		]
 	},
-	// === END ========================================================
+	# END ========================================================
 	{}
 ]
