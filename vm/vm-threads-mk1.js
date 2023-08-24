d3110336e52d4495e57950934213071737de5fec
Maciej Obarski <mobarski@gmail.com>
2023-08-25 00:12:40 +0200

    threads WIP

--- vm/vm-threads-mk1.js
+++ vm/vm-threads-mk1.js
@@ -1,14 +1,15 @@
 // === THREADS MK1 ============================================================
 
 vm.threads = {}
-vm.threads.addr = new Array(vm.cfg.core.threads).fill(0)
-vm.threads.current = -1
+vm.threads.threads = coalesce(vm.cfg.core.threads, 64)
+vm.threads.addr = new Array(vm.threads.threads).fill(0)
+vm.threads.current = 0 // main thread
 
 function vm_threads_set() {
     var id = value_of(get_op())
-    if (id<0) { id = length(vm.threads.addr) + id }
     let addr = get_op()
-    // TODO: thread argument (one value per thread) ???
+    // allow addressing from the end (-1 as the last thread)
+    if (id<0) { id = length(vm.threads.addr) + id }
     vm.threads.addr[id] = addr
 }
 
@@ -18,12 +19,13 @@ function vm_threads_next() {
     for (var i=id+1; i<vm.threads.addr.length; i++) {
         if (vm.threads.addr[i] > 0) {
             vm.ip = vm.threads.addr[i]
-            vm.thread = i
+            vm.threads.current = i
             return
         }
     }
-    // all threads done, wait for the next main loop
-    vm.halt = true
+    // all threads done, return to the main thread
+    vm.ip = vm.threads.addr[0]
+    vm.threads.current = 0
 }
 
 function vm_threads_kill() {
@@ -36,7 +38,7 @@ function vm_threads_id() {
     vm_set(t, vm.threads.current)
 }
 
-vm.ext['thread-set'] = vm_threads_set
-vm.ext['thread-next'] = vm_threads_next
-vm.ext['thread-kill'] = vm_threads_kill
-vm.ext['thread-id'] = vm_threads_id
+vm.ext['threads-set'] = vm_threads_set
+vm.ext['threads-next'] = vm_threads_next
+vm.ext['threads-kill'] = vm_threads_kill
+vm.ext['threads-id'] = vm_threads_id
