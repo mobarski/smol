c931c6997dd34f966d507feb731148211e860a92
Maciej Obarski <mobarski@gmail.com>
2023-08-25 00:11:48 +0200

    threads example

---
+++ examples/threads.asm
@@ -0,0 +1,20 @@
+threads-set 1 :t1
+threads-set 2 :t2
+threads-next
+threads-next
+log 333
+halt
+
+t1:
+	log 111
+	threads-id 11
+	log r11
+	threads-next
+	goto :t1
+
+t2:
+	log 222
+	threads-id 22
+	log r22
+	threads-next
+	goto :t2
