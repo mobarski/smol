308fb36e36e4683d0567fc9f45c690511e948709
Maciej Obarski <mobarski@gmail.com>
2023-09-01 12:04:06 +0200

    timer

---
+++ vm/vm-timer-mk1.js
@@ -0,0 +1,35 @@
+vm.timer = {}
+vm.timer.addr = 0
+vm.timer.freq = 0
+vm.timer.tick = 0
+//
+vm.timer.debug = []
+vm.timer.t0 = 0
+
+function vm_timer_set() {
+    vm.timer.freq = value_of(get_op())
+    vm.timer.addr = get_op()
+    _vm_set_timer()
+}
+
+function _vm_on_timer() {
+    vm.timer.debug.push(performance.now())
+    vm.timer.tick += 1
+    //
+    let addr = vm.timer.addr
+    let freq = vm.timer.freq
+    if ((addr==0) || (freq==0)) { return }
+    _vm_set_timer()
+    //
+    vm.ip = addr
+    vm.halt = false
+    vm_run()
+}
+
+function _vm_set_timer() {
+    if (vm.timer.freq==0) { return }
+    let delay = Math.floor(1000 / vm.timer.freq) // TODO: more precise method (ie 60fps)
+    setTimeout(_vm_on_timer, delay)
+}
+
+vm.ext['timer-set'] = vm_timer_set
